162a0c6e4a2c8924e15ee4e6d206f951d214a975
YOUR_GITHUB_USERNAME <ikhlefanis2010@gmail.com>
2026-07-13 20:25:49 +0100

    Add discord.js dependency

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates
+  ]
 });
 
 
@@ -25,4 +31,51 @@ client.once('ready', () => {
     console.log("Bot joined the voice channel!");
 });
 
+const badWords = [
+  "nami",
+  "97boun",
+  "zbo",
+  "nega3rek",
+  "na7wik",
+  "nikk",
+  "zokk",
+  "rbk",
+  "tiz",
+  "NA3tizmok",
+  "sowa",
+  "7atchoun",
+  "bazol",
+  "ziza",
+  "97ba",
+  "zbi",
+  "zb",
+  "nik mok"
+];
+const LOG_CHANNEL_ID = "1525385648964632637";
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (badWords.some(word => message.content.toLowerCase().includes(word))) {
+
+    await message.delete();
+
+    await message.member.timeout(
+      10 * 60 * 1000,
+      "Bad words"
+    );
+    const logChannel = message.guild.channels.cache.get(LOG_CHANNEL_ID);
+
+if (logChannel) {
+  logChannel.send(
+    `🛡️ **Moderation Log**\n\n` +
+    `👤 User: ${message.author}\n` +
+    `⚠️ Reason: Bad words\n` +
+    `⏱️ Timeout: 10 minutes\n` +
+    `💬 Message: ${message.content}`
+  );
+}
+
+  }
+});
 client.login(process.env.TOKEN);
